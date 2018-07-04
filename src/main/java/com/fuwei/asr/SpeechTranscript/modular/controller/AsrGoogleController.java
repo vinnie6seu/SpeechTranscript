ffc54e316a5a9d10f384937648de92eafcf72ab1
yaoqiaobing <yaoqiaobing@unionpay.com>
2018-07-04 23:04:38 +0800

    测试

--- src/main/java/com/fuwei/asr/SpeechTranscript/modular/controller/AsrGoogleController.java
+++ src/main/java/com/fuwei/asr/SpeechTranscript/modular/controller/AsrGoogleController.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -118,7 +119,7 @@ public class AsrGoogleController extends BaseController {
 //	}
 
 	@PostMapping("/testSdkApi")
-	public Object testSdkApi(User user) throws IOException {
+	public Object testSdkApi(@RequestBody User user) throws IOException {
 		
 		
 		log.debug("test testSdkApi debug log ....");
