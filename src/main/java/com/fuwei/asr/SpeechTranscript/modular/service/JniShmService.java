c141cb80cb8c82936bcd1fd658b746dfcf92a11b
yaoqiaobing <yaoqiaobing@unionpay.com>
2018-07-10 16:36:44 +0800

    测试在jni的c函数中调用java的log打日志

--- src/main/java/com/fuwei/asr/SpeechTranscript/modular/service/JniShmService.java
+++ src/main/java/com/fuwei/asr/SpeechTranscript/modular/service/JniShmService.java
@@ -1,12 +1,16 @@
 package com.fuwei.asr.SpeechTranscript.modular.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class JniShmService {
-	private Logger log = LoggerFactory.getLogger(this.getClass());
+//	private Logger log = LoggerFactory.getLogger(this.getClass());
 	
 	/**
 	 * 加载动态库
